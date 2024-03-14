491b61474a4c1c586f7a757113dfc45b172d45cc
Justin Tran <justin.tran@haw-hamburg.de>
2024-03-14 16:53:19 +0100

    added Hargraeves PET equation

--- methods/meteorological/spei_meteorological.py
+++ methods/meteorological/spei_meteorological.py
@@ -5,45 +5,46 @@ from datetime import datetime, timedelta
 from pyproj import Transformer
 from scipy import stats
 from os.path import exists
+from osgeo import osr
 
 
 def read_asc_file(filepath):
     """Reads an .asc file with a variable header size and returns the data as a numpy array."""
     header_keys = ['NCOLS', 'NROWS', 'XLLCORNER',
                    'YLLCORNER', 'CELLSIZE', 'NODATA_VALUE']
-    asc_header = {}
+    header = {}
 
     with open(filepath, 'r') as file:
         line = file.readline()
         while line:
             parts = line.strip().split()
             if parts[0].upper() in header_keys:
-                asc_header[parts[0].lower()] = float(parts[1]) if parts[0].upper(
+                header[parts[0].lower()] = float(parts[1]) if parts[0].upper(
                 ) != 'NCOLS' and parts[0].upper() != 'NROWS' else int(parts[1])
                 # Break the loop if all necessary header info is collected
-                if len(asc_header) == len(header_keys):
+                if len(header) == len(header_keys):
                     break
             line = file.readline()
 
         # No need to skip rows now, as the file pointer is already at the right position
         data = np.loadtxt(file, skiprows=0)
         # Convert NoData values to NaN
-        data[data == asc_header['nodata_value']] = np.nan
+        data[data == header['nodata_value']] = np.nan
 
-    return data, asc_header
+    return data, header
 
 
-def write_asc_file(data, file_name, asc_header):
+def write_asc_file(data, file_name, header):
     """Writes data to an .asc file using the provided header information."""
 
     try:
         with open(file_name, 'w') as file:
-            file.write(f"NCOLS {asc_header['ncols']}\n")
-            file.write(f"NROWS {asc_header['nrows']}\n")
-            file.write(f"XLLCORNER {asc_header['xllcorner']}\n")
-            file.write(f"YLLCORNER {asc_header['yllcorner']}\n")
-            file.write(f"CELLSIZE {asc_header['cellsize']}\n")
-            file.write(f"NODATA_VALUE {asc_header['nodata_value']}\n")
+            file.write(f"NCOLS {header['ncols']}\n")
+            file.write(f"NROWS {header['nrows']}\n")
+            file.write(f"XLLCORNER {header['xllcorner']}\n")
+            file.write(f"YLLCORNER {header['yllcorner']}\n")
+            file.write(f"CELLSIZE {header['cellsize']}\n")
+            file.write(f"NODATA_VALUE {header['nodata_value']}\n")
             np.savetxt(file, data, fmt="%.4f")
             return True
     except Exception as e:
@@ -61,7 +62,7 @@ def estimate_pet_tw(temperature):
     return PET
 
 
-def calculate_spei(D, asc_header, distribution="ll"):
+def calculate_spei(D, header, distribution="ll"):
     """Calculates SPEI using a log-logistic distribution"""
     if distribution == "ll":
         # Log-logistic distribution
@@ -81,32 +82,30 @@ def calculate_spei(D, asc_header, distribution="ll"):
     spei = stats.norm.ppf(cdf)
     # Replace NaN and inf values with 'nodata_value'
     spei = np.where(np.isnan(spei) | np.isinf(spei),
-                    asc_header['nodata_value'], spei)
+                    header['nodata_value'], spei)
     return spei
 
 
 def estimate_svp(T):
-    """Calculate the estimated saturation vapor pressure (in millibars) for a given temperature (in Celsius) using the Magnus formula."""
-    return 0.61094 * np.exp((17.625 * T) / (T + 243.04))
+    """Calculate the estimated saturation vapor pressure (in millibars) for a given temperature (in Celsius) using the August-Roche-Magnus formula."""
+    return 6.1094 * np.exp((17.625 * T) / (T + 243.04))
     # return 6.112 * np.exp((17.67 * T) / (T + 243.5))
     # return 0.61078 * np.exp(17.269388 * (T) / (T - 35.86))
     # """Calculate the estimated saturation vapor pressure (in millibars) for a given temperature (in Celsius) using the Buck formula. (see https://en.wikipedia.org/wiki/Vapour_pressure_of_water)"""
-    # return 0.61121 * np.exp((18.678 - (T / 234.5)) * (T / 257.14 + T))
-    # return 6.112 * np.exp((17.67 * T) / (T + 243.5))
-    # return 0.61078 * np.exp(17.269388 * (T) / (T - 35.86))
+    # return 6.1121 * np.exp((18.678 - (T / 234.5)) * (T / 257.14 + T))
 
 
-def estimate_net_radiation(Rs_kwh, T_max, T_min, es_mb):
+def estimate_net_radiation(Rs_kwh, T_max, T_min, svp):
     """
     Estimate net radiation using the daily solar radiation, the temperature and the saturation vapor pressure .
 
     Parameters:
-    - Rs_kwh: Daily solar radiation in kWh/m^2.
-    - T_max, T_min: Daily maximum and minimum temperatures in degrees Celsius.
-    - es_mb: Saturation vapor pressure in millibars.
+    - Rs_kwh: Daily solar radiation (kWh/m^2)
+    - T_max, T_min: Daily maximum and minimum temperatures (degrees Celsius)
+    - svp: Saturation vapor pressure (millibars)
 
     Returns:
-    - Rn: Net radiation in MJ/m^2/day.
+    - Rn: Net radiation (MJ/m^2/day)
     """
     # Constants
     sigma = 4.903e-9  # Stefan-Boltzmann constant (MJ K⁻⁴ m⁻² day⁻¹)
@@ -116,7 +115,7 @@ def estimate_net_radiation(Rs_kwh, T_max, T_min, es_mb):
     Rs = Rs_kwh * 3.6  # TODO: use "days_in_month=30.4583" ?
 
     # Convert saturation vapor pressure from millibars to kPa
-    es_kpa = es_mb * 0.1
+    es_kpa = svp * 0.1
 
     # Estimate net shortwave radiation
     Rns = (1 - albedo) * Rs
@@ -124,7 +123,8 @@ def estimate_net_radiation(Rs_kwh, T_max, T_min, es_mb):
     # Estimate net longwave radiation using saturation vapor pressure as a proxy
     T_max_K = T_max + 273.15  # Convert to Kelvin
     T_min_K = T_min + 273.15  # Convert to Kelvin
-    Rnl = sigma * ((T_max_K**4 + T_min_K**4) / 2) * (0.34 - 0.14 * np.sqrt(es_kpa)) * (1.35 * (Rs / (Rs_kwh * 3.6)) - 0.35)
+    Rnl = sigma * ((T_max_K**4 + T_min_K**4) / 2) * (0.34 - 0.14 *
+                                                     np.sqrt(es_kpa)) * (1.35 * (Rs / (Rs_kwh * 3.6)) - 0.35)
 
     # Net radiation
     Rn = Rns - Rnl
@@ -237,6 +237,79 @@ def estimate_pet_pt(Rn, T_mean, G=0, gamma_val=0.066, alpha_val=1.26, days_in_mo
     return PET
 
 
+def coords_to_latlon(x, y):
+    """
+    Convert coordinates from EPSG:31467 to latitude and longitude.
+    """
+    source = osr.SpatialReference()
+    source.ImportFromEPSG(31467)
+    target = osr.SpatialReference()
+    target.ImportFromEPSG(4326)
+    transform = osr.CoordinateTransformation(source, target)
+    lon, lat, _ = transform.TransformPoint(x, y)
+    return lat, lon
+
+
+def calc_ra_for_lat(latitude, month):
+    """
+    Calculate monthly average extraterrestrial radiation (Ra) in MJ/m2/day for a given latitude and month.
+
+    :param latitude: Latitude in degrees.
+    :param month: Month as integer (1 = January, ..., 12 = December).
+    :return: Monthly average Ra in MJ/m2/day.
+    """
+    # Constants
+    Gsc = 0.0820  # Solar constant, MJ/m^2/min
+    dr = [1.000110, 1.034420, 1.068710, 1.102990, 1.037390, 1.000880,
+          0.968290, 0.939180, 0.921490, 0.896320, 0.868010, 0.833390]  # Earth-Sun distance reciprocals for each month
+
+    # Convert latitude to radians
+    phi = np.radians(latitude)
+
+    # Calculate solar declination
+    delta = 0.409 * \
+        np.sin(((2 * np.pi / 365) * ((month - 1) * 30.4 + 10)) - 1.39)
+
+    # Calculate sunset hour angle
+    omega = np.arccos(-np.tan(phi) * np.tan(delta))
+
+    # Calculate daily extraterrestrial radiation for the month
+    Ra = ((24 * 60) / np.pi) * Gsc * dr[month - 1] * (omega * np.sin(phi) * np.sin(delta) +
+                                                      np.cos(phi) * np.cos(delta) * np.sin(omega))
+
+    return Ra
+
+
+def calc_ra(header, month):
+    """
+    Calculate monthly average Ra for each cell in a grid based on header information and month.
+    """
+    ra_array = np.zeros((header['nrows'], header['ncols']))
+
+    for row in range(header['nrows']):
+        for col in range(header['ncols']):
+            # Calculate the center of the cell
+            x = header['xllcorner'] + (col + 0.5) * header['cellsize']
+            y = header['yllcorner'] + (row + 0.5) * header['cellsize']
+
+            # Convert cell center to latitude and longitude
+            lat, _ = coords_to_latlon(x, y)
+
+            # Calculate Ra for the cell based on latitude and month
+            ra_array[row, col] = calc_ra_for_lat(lat, month)
+
+    return ra_array
+
+
+def estimate_pet_hg(T_min, T_max, T_mean, header, month):
+    """
+    Estimate PET using the Hargreaves equation.
+    """
+    Ra = calc_ra(header, month)
+    PET = 0.0023 * Ra * (T_mean + 17.8) * (T_max - T_min)**0.5
+    return PET
+
+
 def calc_spei_n_months(start_date: datetime, end_date: datetime, pet_equation: str = "fao56pm", distribution="ll", calc_1_month=True):
     months_diff = (end_date.year - start_date.year) * 12 + \
         end_date.month - start_date.month + 1
@@ -257,7 +330,7 @@ def calc_spei_n_months(start_date: datetime, end_date: datetime, pet_equation: s
     Rs_kWh_root_path = "/media/jtrvz/1tb/drought_data/radiation/dwd/global"
     Rs_kWh_base_file_name = "grids_germany_monthly_radiation_global_{date}.asc"
 
-    T_min_asc_content, asc_header = load_multi_asc_files(
+    T_min_asc_content, header = load_multi_asc_files(
         start_date, end_date, T_min_root_path, T_min_base_file_name)
     T_max_asc_content, _ = load_multi_asc_files(
         start_date, end_date, T_max_root_path, T_max_base_file_name)
@@ -286,7 +359,7 @@ def calc_spei_n_months(start_date: datetime, end_date: datetime, pet_equation: s
                 Rs_kwh=Rs_kWh_asc_content[curr_date_str],
                 T_min=T_min_asc_content[curr_date_str],
                 T_max=T_max_asc_content[curr_date_str],
-                es_mb=svp,
+                svp=svp,
             )
             pet = estimate_pet_fao56pm(
                 Rn=Rn,
@@ -302,11 +375,11 @@ def calc_spei_n_months(start_date: datetime, end_date: datetime, pet_equation: s
             if months_diff == 1 or calc_1_month:
                 # 1-month SPEI
                 spei = calculate_spei(
-                    D=D_results[curr_date_str], asc_header=asc_header, distribution=distribution)
+                    D=D_results[curr_date_str], header=header, distribution=distribution)
 
                 output_file_path = f"output/1month/spei_fao56pm_{distribution}_{curr_date_str}.asc"
                 ok = write_asc_file(
-                    data=spei, file_name=output_file_path, asc_header=asc_header)
+                    data=spei, file_name=output_file_path, header=header)
                 if ok:
                     print(
                         f"SUCCESS: Saved 1-month SPEI as asc file '{output_file_path}'.")
@@ -315,8 +388,48 @@ def calc_spei_n_months(start_date: datetime, end_date: datetime, pet_equation: s
                         f"ERROR: Failed to save 1-month SPEI as asc file '{output_file_path}'.")
 
                 # Plot
-                plot_spei(spei, asc_header,
+                plot_spei(spei, header,
                           f"output/1month/spei_fao56pm_{distribution}_{curr_date_str}.png")
+
+            # Jump to next month
+            next_month = curr_date.replace(day=28) + timedelta(days=4)
+            curr_date = next_month - timedelta(days=next_month.day - 1)
+    elif pet_equation == "hg":
+        pet_results = {}
+        D_results = {}
+
+        curr_date = start_date
+        while curr_date <= end_date:
+            curr_date_str = curr_date.strftime('%Y%m')
+
+            pet = estimate_pet_hg(T_min=T_min_asc_content[curr_date_str],
+                                  T_max=T_max_asc_content[curr_date_str],
+                                  T_mean=T_mean_asc_content[curr_date_str],
+                                  header=header,
+                                  month=curr_date.month,
+                                  )
+
+            pet_results[curr_date.strftime('%Y%m')] = pet
+            D_results[curr_date.strftime('%Y%m')] = prec_asc_content[curr_date_str] - pet
+
+            if months_diff == 1 or calc_1_month:
+                # 1-month SPEI
+                spei = calculate_spei(
+                    D=D_results[curr_date_str], header=header, distribution=distribution)
+
+                output_file_path = f"output/1month/spei_{pet_equation}_{distribution}_{curr_date_str}.asc"
+                ok = write_asc_file(
+                    data=spei, file_name=output_file_path, header=header)
+                if ok:
+                    print(
+                        f"SUCCESS: Saved 1-month SPEI as asc file '{output_file_path}'.")
+                else:
+                    print(
+                        f"ERROR: Failed to save 1-month SPEI as asc file '{output_file_path}'.")
+
+                # Plot
+                plot_spei(spei, header,
+                          f"output/1month/spei_{pet_equation}_{distribution}_{curr_date_str}.png")
 
             # Jump to next month
             next_month = curr_date.replace(day=28) + timedelta(days=4)
@@ -334,15 +447,15 @@ def calc_spei_n_months(start_date: datetime, end_date: datetime, pet_equation: s
 
             # Calculate SPEI
             spei = calculate_spei(
-                D=D_sum, asc_header=asc_header, distribution=distribution)
+                D=D_sum, header=header, distribution=distribution)
 
             # Save n-month SPEI as asc file
             total_months = len(D_results)
-            output_file_path = f"output/{total_months}month/spei_fao56pm_{distribution}_{curr_date_str}.asc"
+            output_file_path = f"output/{total_months}month/spei_{pet_equation}_{distribution}_{curr_date_str}.asc"
             ok = write_asc_file(
                 data=spei,
                 file_name=output_file_path,
-                asc_header=asc_header,
+                header=header,
             )
             if ok:
                 print(
@@ -352,11 +465,11 @@ def calc_spei_n_months(start_date: datetime, end_date: datetime, pet_equation: s
                     f"ERROR: Failed to save {total_months}-month SPEI as asc file '{output_file_path}'.")
 
             # Save n-month Plot as png file
-            plot_spei(spei, asc_header,
-                      f"output/{total_months}month/spei_fao56pm_{distribution}_{curr_date_str}.png")
+            plot_spei(spei, header,
+                      f"output/{total_months}month/spei_{pet_equation}_{distribution}_{curr_date_str}.png")
 
 
-def plot_spei(spei_data, asc_header, save_path=""):
+def plot_spei(spei_data, header, save_path=""):
     # Mask the NoData values
     spei_data_masked = np.ma.masked_where(spei_data == -999, spei_data)
 
@@ -370,11 +483,11 @@ def plot_spei(spei_data, asc_header, save_path=""):
 
     # Calculate the extent using the asc header info
     x_min, y_min, x_max, y_max = transform_coordinates(
-        asc_header['xllcorner'], asc_header['yllcorner'],
-        asc_header['xllcorner'] +
-        asc_header['ncols'] * asc_header['cellsize'],
-        asc_header['yllcorner'] +
-        asc_header['nrows'] * asc_header['cellsize'],
+        header['xllcorner'], header['yllcorner'],
+        header['xllcorner'] +
+        header['ncols'] * header['cellsize'],
+        header['yllcorner'] +
+        header['nrows'] * header['cellsize'],
         'EPSG:31467', 'EPSG:4326'
     )
     extent = [x_min, x_max, y_min, y_max]
@@ -384,7 +497,7 @@ def plot_spei(spei_data, asc_header, save_path=""):
                cmap=spei_cmap,
                norm=spei_norm,
                extent=extent,
-               # extent=[0, asc_header['ncols'], 0, asc_header['nrows']]
+               # extent=[0, header['ncols'], 0, header['nrows']]
                )
     plt.colorbar(label='SPEI Value')
     plt.title('SPEI Values for Germany')
@@ -407,30 +520,32 @@ def transform_coordinates(x_min, y_min, x_max, y_max, src_crs, dest_crs):
 
 
 def load_multi_asc_files(start_date: datetime, end_date: datetime, root_path, base_file_name: str):
-    asc_header = {}
+    header = {}
     asc_file_content = {}
     current_date: datetime = start_date
     while current_date <= end_date:
         file_name = base_file_name.format(date=current_date.strftime('%Y%m'))
         full_path = f"{root_path}/{file_name}".replace("//", "/")
         if exists(full_path):
-            if asc_header:
+            if header:
                 asc_file_content[current_date.strftime(
                     '%Y%m')], _ = read_asc_file(full_path)
             else:
                 asc_file_content[current_date.strftime(
-                    '%Y%m')], asc_header = read_asc_file(full_path)
+                    '%Y%m')], header = read_asc_file(full_path)
 
         next_month = current_date.replace(day=28) + timedelta(days=4)
         current_date = next_month - timedelta(days=next_month.day - 1)
 
-    return asc_file_content, asc_header
+    return asc_file_content, header
 
 
 ########################################
 start = datetime(2022, 1, 1)
-end = datetime(2022, 12, 1)
+end = datetime(2022, 3, 1)
 calc_spei_n_months(start_date=start, end_date=end,
-                   distribution="g", calc_1_month=False)
+                   pet_equation="hg",distribution="g",
+                   calc_1_month=True)
 calc_spei_n_months(start_date=start, end_date=end,
-                   distribution="ll", calc_1_month=False)
+                   pet_equation="hg",distribution="ll",
+                   calc_1_month=True)
